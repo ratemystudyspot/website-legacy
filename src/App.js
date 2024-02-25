6a8a78666ffb93c1c6b417bd918e66e872d8db10
LeoShangTang <leoshang12@gmail.com>
2024-02-25 13:04:20 -0800

    add imports

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LoginForm from './Components/AuthForm/LoginForm';
 import RegisterForm from './Components/AuthForm/RegisterForm';
 import StudySpotCard from './Components/StudyCard/StudySpotCard';
 import ButtonComponent from './Components/ButtonComponent';
+import StudySpots from './SampleData/StudySpots';
+import ListOfStudySpotCards from './Components/StudyCard/ListOfStudySpotCards';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
