a5c0abb0cf93eed7a6a366da2eec066dbcfd75cb
Ray Zhou <rayzhou4@gmail.com>
2024-06-07 19:28:00 -0600

    small changes: -update studyspot card -changed the filters

--- src/Components/StudyCard/SpotCardsFilter.jsx
+++ src/Components/StudyCard/SpotCardsFilter.jsx
@@ -2,16 +2,19 @@ import StudySpots from "../../SampleData/StudySpots";
 import StudySpotCard from "./StudySpotCard";
 import "./SpotCardsFilter.css";
 import { BiVolumeMute } from "react-icons/bi";
-import { TbSofa, TbBatteryCharging2, TbLockOpen } from "react-icons/tb";
-import { MdOutlineGroupOff, MdOutlineDoorFront  } from "react-icons/md";
+import { TbSofa, TbBatteryCharging2, TbLockOpen, TbMicrowave  } from "react-icons/tb";
+import { MdOutlineFastfood, MdOutlineDoorFront,   } from "react-icons/md";
 
 const SpotCardsFilter = ({ filterSelected, onFilterSelect }) => {
   const filterOptions = [
     { label: 'Quiet', value: 'quiet', icon: <BiVolumeMute size={20} className="filter-icon"/> },
-    { label: 'Comfy', value: 'comfy', icon: <TbSofa size={20} className="filter-icon"/> },
-    { label: 'Not busy', value: 'not-busy', icon: <MdOutlineGroupOff size={20} className="filter-icon"/> },
+    // { label: 'Comfy', value: 'comfy', icon: <TbSofa size={20} className="filter-icon"/> },
+    // { label: 'Not busy', value: 'not-busy', icon: <MdOutlineGroupOff size={20} className="filter-icon"/> },
     { label: 'Outlets', value: 'outlets', icon: <TbBatteryCharging2 size={20} className="filter-icon"/> },
+    { label: 'Microwaves', value: 'microwaves', icon: <TbMicrowave  size={20} className="filter-icon"/> },
+    { label: 'Restaurants', value: 'restaurants', icon: <MdOutlineFastfood  size={20} className="filter-icon"/> },
     { label: 'Open Now', value: 'open-now', icon: <MdOutlineDoorFront size={20} className="filter-icon"/> },
+    
     // Add more filter options as needed
   ];
 
