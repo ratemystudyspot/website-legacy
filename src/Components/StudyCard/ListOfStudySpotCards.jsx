12c10142ca9be36b5c03331f94cac695f1214149
Ray Zhou <rayzhou4@gmail.com>
2024-06-17 17:29:05 -0600

    built open now filter feature

--- src/Components/StudyCard/ListOfStudySpotCards.jsx
+++ src/Components/StudyCard/ListOfStudySpotCards.jsx
@@ -1,19 +1,56 @@
 import StudySpots from "../../SampleData/StudySpots";
 import StudySpotCard from "./StudySpotCard";
 import "./ListOfStudySpotCards.css";
-import { getSpots, getSpotsByFeatures } from "../../Services/studySpot";
+import { getSpots, getSpotsByFeatures, getSpotsByTime } from "../../Services/studySpot";
 import { useEffect, useState } from "react";
+import { GiConsoleController } from "react-icons/gi";
 
 const ListOfStudySpotCards = ({ filterSelected, currentLocation }) => {
   const [cards, setCards] = useState([]);
 
+  const convertTimeTo24h = (time12h) => {
+    const [time, modifier] = time12h.split(' ');
+    let [hours, minutes, seconds] = time.split(':');
+
+    if (hours === '12') {
+      hours = '00';
+    }
+    if (modifier === 'PM') {
+      hours = parseInt(hours, 10) + 12;
+    }
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  const getCurrentDate = () => {
+    const date = new Date();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDayOfWeek = daysOfWeek[date.getDay()];
+    const currentTime12h = date.toLocaleTimeString();
+    const currentTime24h = convertTimeTo24h(currentTime12h);
+    return { currentDayOfWeek, currentTime24h };
+  }
+
   useEffect(() => {
     const getFilteredCards = async () => {
       try {
-        const filteredCards = await getSpotsByFeatures(filterSelected);
-        const allCards = await getSpots();
+        const featureCards =
+          (filterSelected.length === 0 || (filterSelected.length === 1 && filterSelected.includes('open-now')))
+            ? await getSpots()
+            : await getSpotsByFeatures(filterSelected.filter(filter => filter !== 'open-now')); // get all spots with all selected filters (excluding open-now filter)
 
-        (filterSelected.length === 0) ? setCards(allCards) : setCards(filteredCards);
+        if (filterSelected.includes('open-now')) { // if open-now selected, find opened spots PLUS with given filters 
+          const { currentDayOfWeek, currentTime24h } = getCurrentDate();
+          const openCards = await getSpotsByTime(currentDayOfWeek, currentTime24h);
+          
+          const featureCardsIDs = featureCards.map(featureCard => featureCard.id) // get ids of all spots from filters (excluding open-now filter)
+          const filteredCards = openCards.filter(openCard => featureCardsIDs.includes(openCard.id)) // get all opened spots AND with given filters (excluding open-now filter)
+
+          console.log("FILTERED CARDS:", filteredCards)
+          setCards(filteredCards);
+        } else { // if code reaches this line, means that all cards are shown
+          const allCards = featureCards; 
+          setCards(allCards);
+        }
       } catch (error) {
         console.error("Error fetching filtered cards:", error);
       }
--- src/Components/StudyCard/SpotCardsFilter.jsx
+++ src/Components/StudyCard/SpotCardsFilter.jsx
@@ -21,14 +21,14 @@ const SpotCardsFilter = ({ filterSelected, onFilterSelect }) => {
   ];
 
   const selected = (value) => {
-    return filterSelected.includes(value);
+    return filterSelected.includes(value); // returns true if the filter is included in the selected filter list, otherwise false
   }
 
   const handleFilterSelect = (option) => {
     onFilterSelect((prevFilters) =>
       selected(option.value)
-        ? prevFilters.filter((filter) => filter !== option.value)
-        : prevFilters.concat([option.value]));
+        ? prevFilters.filter((filter) => filter !== option.value) // if filter is selected, remove from selected filters
+        : prevFilters.concat([option.value])); // if filter is not selected, add to seelcted filters
   };
 
   return (
