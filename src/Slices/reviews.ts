76766abe546e779a03bdac8e295e55a8b82eac4b
LeoShangTang <leoshang12@gmail.com>
2024-07-02 19:38:47 -0600

    General Reviews Slice finished

--- src/Slices/reviews.ts
+++ src/Slices/reviews.ts
@@ -1,6 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from '../store';
 
+// ------------------ REVIEWS'S SCHEMA ---------------------------------
 export interface Review {
   id: number;
   user_id: number;
@@ -18,8 +19,8 @@ export interface ReviewsState {
   value: Array<Review>;
 }
 
-const initialState = {
-  values: []
+let initialState: ReviewsState = {
+  value: [],
 };
 
 export const reviewsSlice = createSlice({
@@ -27,12 +28,30 @@ export const reviewsSlice = createSlice({
   initialState,
   reducers: {
     // TODO: add reviews reducers
+    addReview: (state, action: PayloadAction<{review: Review}>) => {
+      const { review } = action.payload;
+      state.value = [review , ... state.value];
+    },
+    removeReview: (state, action: PayloadAction<{review_id: number}>) => {
+      const { review_id } = action.payload;
+      state.value = state.value.filter((review) => review.id !== review_id);
+    },
+    editReview: (state, action: PayloadAction<{updatedData: Review}>) => {
+      const { updatedData } = action.payload
+      const reviewIndex = state.value.findIndex((review) => review.id === updatedData.id);
+      if (reviewIndex !== -1) {
+        state.value[reviewIndex] = {...state.value[reviewIndex], ...updatedData};
+      }
+    }
   }
 })
 
 // export const { ... } = reviewsSlice.actions TEMPLATE, REPLACE WITH REDUCERS
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.reviews.values;
+// export const selectCount = (state: RootState) => state.reviews.values;
+
+
+export const {addReview, removeReview, editReview} = reviewsSlice.actions;
 
 export default reviewsSlice.reducer;
