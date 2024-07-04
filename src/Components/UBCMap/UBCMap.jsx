ba8e5b56528d3cb359ecf4fb8b74562032de84f9
Ray Zhou <rayzhou4@gmail.com>
2024-07-03 18:09:59 -0600

    styling fix for ubc map

--- src/Components/UBCMap/UBCMap.jsx
+++ src/Components/UBCMap/UBCMap.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import './UBCMap.scss';
 
 const UBCMap = ({ markers, mapWidth = '500px', mapHeight = '500px', mapCenter = [-123.2460, 49.2626], mapZoom = 13 }) => {
     const mapContainer = useRef(null);
@@ -19,8 +20,18 @@ const UBCMap = ({ markers, mapWidth = '500px', mapHeight = '500px', mapCenter = 
 
         if (markers) {
             markers.forEach((marker) => { // loop through all markers and place onto map (with popup if needed)
-                const popup = (marker?.label) ? new maplibregl.Popup().setText(marker.label) : null;
-                new maplibregl.Marker().setLngLat(marker.coordinates).addTo(map).setPopup(popup);
+                const popup = (marker?.label)
+                    ? new maplibregl.Popup({
+                        className: "ubc-map__popup",
+                        closeButton: false,
+                    })
+                        .setText(marker.label)
+                    : null;
+
+                new maplibregl.Marker({ className: "ubc-map__marker" })
+                    .setLngLat(marker.coordinates)
+                    .setPopup(popup)
+                    .addTo(map);
             })
         }
 
