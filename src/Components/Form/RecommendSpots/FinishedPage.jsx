769f91ddc013fe43d9c92b793aebcac840b04f3c
Ray Zhou <rayzhou4@gmail.com>
2024-07-08 12:00:35 -0600

    fixed scss and classnames

--- src/Components/Form/RecommendSpots/FinishedPage.jsx
+++ src/Components/Form/RecommendSpots/FinishedPage.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "./LandingFinishedPage.scss";
 
 // TODO: send info to us somehow
 function FinishedPage() {
   return (
-    <div className="landing-page-box">
-    <div className="landing-page-box__left-container">
-      <div className="landing-page-box__description-container">
-        <h1 className="landing-page-box__title">
+    <div className="finished-page-box">
+    <div className="finished-page-box__left-container">
+      <div className="finished-page-box__description-container">
+        <h1 className="finished-page-box__title">
           You're finished!
         </h1>
-        <p className="landing-page-box__description-text">
+        <p className="finished-page-box__description-text">
           Make sure to review all the information one more time just to confirm everything before you submit your form. Thank you for suggesting a spot, our team will get back to as soon as possible, so keep an eye out on your inbox!
         </p>
       </div>
     </div>
-    <div className="landing-page-box__right-container">
-      <div className="landing-page-box__image-container">
+    <div className="finished-page-box__right-container">
+      <div className="finished-page-box__image-container">
         <img
-          className="landing-page-box__image"
+          className="finished-page-box__image"
           src={require("../../Assets/people-celebrating.png")}
           alt="isometic library vector"
         />
--- src/Components/Form/RecommendSpots/LandingPage.jsx
+++ src/Components/Form/RecommendSpots/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./LandingPage.scss";
+import "./LandingFinishedPage.scss";
 
 function LandingPage() {
   return (
