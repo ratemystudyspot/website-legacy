0e4aaf95dcfbca02e93dfdc979c33ed76e37de53
LeoShangTang <leoshang12@gmail.com>
2024-07-22 22:03:51 -0600

    Review Control Panel Initialization

---
+++ src/Components/Review/ReviewControlPanel.jsx
@@ -0,0 +1,20 @@
+import './ReviewControlPanel.scss'
+
+const ReviewControlPanel = () => {
+
+    return (
+        <div className='review-control-panel-box'>
+            <div className='review-control-panel-box__search'>
+
+            </div>
+            <div className='review-control-panel-box__filter'>
+                
+            </div>
+            <div className='review-control-panel-box__sort-by'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default ReviewControlPanel
