baff90ffa11357d04c6a195fa0c00f40dcb8fd9b
Ray Zhou <rayzhou4@gmail.com>
2024-07-23 21:26:59 -0600

    revert commit
    Signed-off-by: Ray Zhou <rayzhou4@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import { React, useState, useEffect } from 'react'
+import React, { lazy, Suspense, useState, useEffect, useRef } from 'react';
 import './App.css';
-import RegisterForm from './Pages/AuthForm/RegisterForm';
-import Homepage from './Pages/Homepage';
-import ErrorPage from './Pages/Structure/Errorpage';
-import Aboutpage from './Pages/Aboutpage';
-import RecommendSpotspage from './Pages/RecommendSpotspage';
-import Seekpage from './Pages/Seekpage'
-import Verify from './Pages/AuthForm/Verify';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Layout from './Pages/Structure/Layout';
-import RequireAuth from './Pages/Structure/RequireAuth';
-import Unauthorized from './Pages/Structure/Unauthorized';
-import SpotDetail from './Pages/SpotDetailpage';
-import Reset from './Pages/AuthForm/PasswordRecovery/ResetPage';
-import AuthForm from './Pages/AuthForm/AuthForm';
-import UserSettings from './Pages/UserSettingspage';
 import useAuth from "./hooks/useAuth";
 import { handleRefreshToken } from './Services/auth';
 import { jwtDecode } from "jwt-decode";
+import { useAppDispatch } from './hooks.ts';
+import { clearReviews } from './Slices/reviews.ts';
+// Lazy load components
+const RequireAuth = lazy(() => import('./Pages/Structure/RequireAuth'));
+const RegisterForm = lazy(() => import('./Pages/AuthForm/RegisterForm'));
+const Homepage = lazy(() => import('./Pages/Homepage'));
+const ErrorPage = lazy(() => import('./Pages/Structure/Errorpage'));
+const Aboutpage = lazy(() => import('./Pages/Aboutpage'));
+const RecommendSpotspage = lazy(() => import('./Pages/RecommendSpotspage'));
+const Seekpage = lazy(() => import('./Pages/Seekpage'));
+const Verify = lazy(() => import('./Pages/AuthForm/Verify'));
+const Unauthorized = lazy(() => import('./Pages/Structure/Unauthorized'));
+const SpotDetail = lazy(() => import('./Pages/SpotDetailpage'));
+const Reset = lazy(() => import('./Pages/AuthForm/PasswordRecovery/ResetPage'));
+const AuthForm = lazy(() => import('./Pages/AuthForm/AuthForm'));
+const UserSettings = lazy(() => import('./Pages/UserSettingspage'));
 
 function App() {
   // init roles
   const ROLES = {
-    'User': process.env.REACT_APP_USER_ROLE,
-  }
+    User: process.env.REACT_APP_USER_ROLE,
+  };
 
   const { auth, setAuth } = useAuth();
   const [authLoaded, setAuthLoaded] = useState(false);
+  const [appLoaded, setAppLoaded] = useState(false);
+  
+  const dispatch = useAppDispatch();
 
-  // on reload, fetch refresh token
   useEffect(() => {
     const fetchRefreshToken = async () => {
       try {
-        let response = await handleRefreshToken();
-        let accessToken = response.access_token;
-        let accessTokenDecoded = jwtDecode(accessToken); // decode access token
+        var response = await handleRefreshToken();
+        var accessToken = response.access_token;
+        var accessTokenDecoded = jwtDecode(accessToken);
 
         setAuth({ access_token: accessToken, user_info: accessTokenDecoded?.UserInfo }); // authorize user
 
@@ -44,46 +49,67 @@ function App() {
         accessTokenDecoded = null;
       } catch (error) { // 2 scenarios: 1) user hasn't receive RT, about to login 2) expired RT
         console.error(error);
+        setAppLoaded(true);
       }
     }
     fetchRefreshToken();
   }, []);
 
   // use to check if auth has been recieved and can be readily used in app
-  useEffect(() => {  
-    if (!authLoaded && Object.keys(auth).length !== 0) setAuthLoaded(true); // only use when authLoaded is false to prevent redundant reloads and when auth has information to ensure user has been authenticated
-  }, [auth])
+  useEffect(() => {
+    if (!authLoaded && Object.keys(auth).length !== 0) { // only use when authLoaded is false to prevent redundant reloads and when auth has information to ensure user has been authenticated
+      setAuthLoaded(true)
+      setAppLoaded(true);
+      return;
+    }
+  }, [auth]);
+
+  // clear redux state after exiting study spot detailed page
+  let location = useLocation();
+  let prevLocation = useRef(location);
+  useEffect(() => {
+    // Function to check if a path matches a wildcard pattern
+    const matchesWildcard = (path, pattern) => {
+      const regex = new RegExp(`^${pattern.replace('*', '.*')}$`);
+      return regex.test(path);
+    };
+    
+    if (matchesWildcard(prevLocation.current.pathname, '/spots/*')) {
+      dispatch(clearReviews());
+    } 
+    
+    prevLocation.current = location;
+  }, [location]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* public routes */}
-        <Route index element={<Homepage />} />
-        <Route path="about" element={<Aboutpage />} />
-        <Route path="spots/seek-a-spot" element={<Seekpage />} />
-        <Route path="login" element={<AuthForm />} />
-        <Route path="signup" element={<RegisterForm />} /> { /* MAYBE DELETE */}
-        {/* <Route path="verify/:userId" element={<Verify />} /> !!!future todo*/}
-        <Route path="unauthorized" element={<Unauthorized />} />
-        <Route path="spots/:id" element={<SpotDetail />} />
+    <Suspense fallback={<></>}>
+      {appLoaded && // used to only load everything once pre-load things such as handling authorization has been complete
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* public routes */}
+            <Route index element={<Homepage />} />
+            <Route path="about" element={<Aboutpage />} />
+            <Route path="spots/seek-a-spot" element={<Seekpage />} />
+            <Route path="login" element={<AuthForm />} />
+            <Route path="signup" element={<RegisterForm />} />
+            {/* <Route path="verify/:userId" element={<Verify />} /> !!!future todo*/}
+            <Route path="unauthorized" element={<Unauthorized />} />
+            <Route path="spots/:id" element={<SpotDetail />} />
 
-        {/* protected routes */}
-        <Route element={<RequireAuth authLoaded={authLoaded} setAuthLoaded={setAuthLoaded} allowedRoles={[ROLES.User]} />}>
-          <Route path="spots/suggest-a-spot" element={<RecommendSpotspage />} />
-          <Route path="user/settings" element={<UserSettings />} />
-        </Route>
-
-        {/* !!! protect these routes so that you cannot just access these pages */}
-        <Route path="password/:token" element={<Reset />} />
-        {/* <Route path="forgot-password/OTP" element={<OTPForm />} />  
-        <Route path="forgot-password/recovered" element={<Recovered />} />  */}
-
-
-        {/* error page */}
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
-    </Routes>
+            {/* protected routes */}
+            <Route element={<RequireAuth authLoaded={authLoaded} setAuthLoaded={setAuthLoaded} allowedRoles={[ROLES.User]} />}>
+              <Route path="spots/suggest-a-spot" element={<RecommendSpotspage />} />
+              <Route path="user/settings" element={<UserSettings />} />
+            </Route>
+            {/* password reset */}
+            <Route path="password/:token" element={<Reset />} />
+            {/* error page */}
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      }
+    </Suspense>
   );
-}
+};
 
 export default App;
