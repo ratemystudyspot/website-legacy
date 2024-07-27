eee2c1eb07fc95b8ccb51367a4b48c63405e33c4
LeoShangTang <leoshang12@gmail.com>
2024-07-26 21:05:41 -0600

    Sliding animation and fixed review card on small screens

--- src/Components/Review/ReviewControlPanel.jsx
+++ src/Components/Review/ReviewControlPanel.jsx
@@ -4,43 +4,31 @@ import { FaSearch } from 'react-icons/fa';
 
 const ReviewControlPanel = () => {
 
-    const [searchTerm, setSearchTerm] = useState(''); 
-    const [showSearchBar, setShowSearchBar] = useState(false);
-    const handleChange = (e) => {
-      setSearchTerm(e.target.value.toLowerCase());
-    };
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
 
-    const handleSearch = () => {
-        console.log("TO BE DONE!");
-    }
+  const handleSearch = () => {
+    console.log("TO BE DONE!");
+  }
 
-    const handleToggleSearchBar = () => [
+  return (
+    <div className='review-control-panel-box'>
 
-    ]
+      <form className="review-control-panel-box__search-box" onSubmit={handleSearch}>
+        <button type="submit" className='review-control-panel-box__search-button'><FaSearch className="review-control-panel-box__icon" /></button>
+        <input className="review-control-panel-box__input-search" type="text" value={searchTerm} onChange={handleChange} />
+      </form>
 
-    return (
-        <div className='review-control-panel-box'>
-            {(showSearchBar) ? 
-            <form className="review-control-panel-box__search-box" onSubmit={handleSearch}>
-                <input
-                  className="review-control-panel-box__input-search"
-                  type="text"
-                  value={searchTerm}
-                  onChange={handleChange}
-                  placeholder="Search reviews"
-                />
-                <button type="submit" className='review-control-panel-box__search-button--open'><FaSearch className="review-control-panel-box__icon" /></button>
-              </form> :
-               <button className='review-control-panel-box__search-button--closed'><FaSearch className="review-control-panel-box__icon" /></button>
-              }
-            <div className='review-control-panel-box__filter'>
-                
-            </div>
-            <div className='review-control-panel-box__sort-by'>
-                
-            </div>
-        </div>
-    )
+      <div className='review-control-panel-box__filter'>
+
+      </div>
+      <div className='review-control-panel-box__sort-by'>
+
+      </div>
+    </div>
+  )
 }
 
 export default ReviewControlPanel
